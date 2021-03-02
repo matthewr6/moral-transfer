8f00de1c212719c29f5d433f14d884916d8fce73
Matthew <mradovan@stanford.edu>
2021-03-01 21:42:17 -0700

    example generator

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,8 +1,9 @@
 import glob
 import pickle
-import ujson as json
+import json
 import os
 from tqdm import tqdm
+import multiprocessing
 
 from nela_features.nela_features import NELAFeatureExtractor, MORAL_FOUNDATION_DICT
 from transformers import BartTokenizerFast, BertTokenizerFast
@@ -12,7 +13,7 @@ from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
 moral_foundations = sorted(MORAL_FOUNDATION_DICT.keys())
-print(moral_foundations)
+print(moral_foundations, '\n')
 
 nela = NELAFeatureExtractor()
 extractor = nela.extract_moral
@@ -33,7 +34,7 @@ def add_headline_moral_features(data):
     preprocessed = []
     for article in tqdm(data):
         try:
-            feature_vector, feature_names = extractor(article['content']) 
+            feature_vector, feature_names = extractor(article['title']) 
         except:
             continue
         feature_dict = dict(zip(feature_names, feature_vector))
@@ -80,7 +81,18 @@ def bart_encoding(data):
 def cnn_bart_encoding(data):
     tokenizer = BartTokenizerFast.from_pretrained('facebook/bart-large-cnn')
     texts = [a['content'] for a in data]
-    encodings = tokenizer(texts, truncation=True, max_length=1024, padding=True, return_attention_mask=True, return_token_type_ids=True)
+    # encodings = tokenizer(texts, truncation=True, max_length=1024, padding=True, return_attention_mask=True, return_token_type_ids=True)
+    encodings = tokenizer(texts, truncation=True, max_lenth=128, padding=True, return_attention_mask=True, return_token_type_ids=True)
+    for idx, article in enumerate(tqdm(data)):
+        article['content'] = encodings.data['input_ids'][idx]
+        article['attention_mask'] = encodings.data['attention_mask'][idx]
+        article['token_type_ids'] = encodings.data['token_type_ids'][idx]
+    return data
+
+def short_cnn_bart_encoding(data):
+    tokenizer = BartTokenizerFast.from_pretrained('facebook/bart-large-cnn')
+    texts = [a['content'] for a in data]
+    encodings = tokenizer(texts, truncation=True, max_lenth=128, padding=True, return_attention_mask=True, return_token_type_ids=True)
     for idx, article in enumerate(tqdm(data)):
         article['content'] = encodings.data['input_ids'][idx]
         article['attention_mask'] = encodings.data['attention_mask'][idx]
@@ -106,10 +118,11 @@ def apply_preprocessing(infile, method, outfile):
         print('{} does not exist; skipping\n'.format(infile))
         return
     print('Loading...')
-    with open(infile, 'r') as f:
-        if 'json' in infile:
+    if 'json' in infile:
+        with open(infile, 'r') as f:
             data = json.load(f)
-        else:
+    else:
+        with open(infile, 'rb') as f:
             data = pickle.load(f)
     print('Preprocessing...')
     preprocessed = method(data)
@@ -119,25 +132,8 @@ def apply_preprocessing(infile, method, outfile):
         pickle.dump(preprocessed, f, pickle.HIGHEST_PROTOCOL)
     print('')
 
-# Multiple hours
-# Add moral features
-# apply_preprocessing('nela-covid-2020/combined/unprocessed.json', add_moral_features, 'nela-covid-2020/combined/moral_features.json')
-apply_preprocessing('nela-covid-2020/combined/pared.json', add_headline_moral_features, 'nela-covid-2020/combined/headlines.pkl')
-apply_preprocessing('nela-covid-2020/combined/headlines.pkl', distilbert_encoding_encoding, 'nela-covid-2020/combined/headlines_distilbert.pkl')
-apply_preprocessing('nela-covid-2020/combined/headlines.pkl', cnn_bart_encoding, 'nela-covid-2020/combined/headlines_cnn_bart.pkl')
+# source = 'nela-elections-2020/combined' # headline max len: 109
+source = 'nela-covid-2020/combined' 
 
-# Seconds
-# Remove irrelevant info to scale down file size
-# apply_preprocessing('nela-covid-2020/combined/moral_features.json', strip_irrelevant_content, 'nela-covid-2020/combined/pared.json')
-
-# # ???
-# # Encoding for pretrained BART
-# apply_preprocessing('nela-covid-2020/combined/pared.json', bart_encoding, 'nela-covid-2020/combined/bart_encodings.pkl')
-# # Encoding for pretrained BERT
-# apply_preprocessing('nela-covid-2020/combined/pared.json', distilbert_encoding, 'nela-covid-2020/combined/distilbert_encodings.pkl')
-# # CNN Bart
-# apply_preprocessing('nela-covid-2020/combined/pared.json', cnn_bart_encoding, 'nela-covid-2020/combined/cnn_bart_encodings.pkl')
-
-# # ~30 min
-# # Custom byte pair encoding
-# apply_preprocessing('nela-covid-2020/combined/pared.json', byte_pair_encoding, 'nela-covid-2020/combined/custom_bpe.pkl')
+apply_preprocessing('{}/unprocessed.pkl'.format(source), add_headline_moral_features, '{}/headlines.pkl'.format(source))
+apply_preprocessing('{}/headlines.pkl'.format(source), short_cnn_bart_encoding, '{}/headlines_cnn_bart.pkl'.format(source))
--- models/generator.py
+++ models/generator.py
@@ -8,7 +8,8 @@ from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader, random_split
 import pytorch_lightning as pl
 
-from transformers import DistilBertModel
+from transformers import DistilBertModel, BartModel
+from transformers import BartTokenizerFast, BertTokenizerFast
 
 # Inputs:
 #   - input sequence (to encoder)
@@ -27,27 +28,35 @@ from transformers import DistilBertModel
 
 # Stacks moral vector with encoded representation, prior to decoder.
 class MoralTransformer(pl.LightningModule):
-    seq_len = 512
 
-    def __init__(self, n_intermediate=512, moral_vec_size=5):
+    def __init__(self, seq_len=1024, n_intermediate=512, moral_vec_size=5):
         super().__init__()
+        self.seq_len = seq_len
+        # self.tokenizer = BertTokenizerFast.from_pretrained('distilbert-base-uncased')
+        self.tokenizer = BartTokenizerFast.from_pretrained('facebook/bart-large-cnn')
+
         # Load pretrained model
-        self.pretrained = DistilBertModel.from_pretrained('distilbert-base-uncased')
-        self.n_vocab = self.pretrained.embeddings.word_embeddings.num_embeddings
-        self.n_encoder_features = self.pretrained.transformer.layer[-1].output_layer_norm.normalized_shape[0]
+        # self.pretrained = DistilBertModel.from_pretrained('distilbert-base-uncased').cuda()
+        self.pretrained = BartModel.from_pretrained('facebook/bart-large-cnn')
+
+        # self.n_vocab = self.pretrained.embeddings.word_embeddings.num_embeddings
+        # self.n_encoder_features = self.pretrained.transformer.layer[-1].output_layer_norm.normalized_shape[0]
+        self.n_vocab = self.pretrained.shared.num_embeddings
+        self.n_encoder_features = self.pretrained.encoder.layernorm_embedding.normalized_shape[0]
 
         # Linear layer to combine encodings and moral features
         self.linear = nn.Linear(self.n_encoder_features + moral_vec_size, n_intermediate)
 
         # Decoder
-        decoder_layer = nn.TransformerDecoderLayer(d_model=n_intermediate, nhead=8, dim_feedforward=1024, dropout=0.1, activation='relu')
-        self.decoder = nn.TransformerDecoder(decoder_layer, num_layers=6)
+        decoder_layer = nn.TransformerDecoderLayer(d_model=n_intermediate, nhead=16, dim_feedforward=1024, dropout=0.1, activation='relu')
+        self.decoder = nn.TransformerDecoder(decoder_layer, num_layers=12)
         self.decoder_head = nn.Linear(n_intermediate, self.n_vocab)
 
     def forward(self, source, moral_target, generated):
         copied_morals = torch.unsqueeze(moral_target, 1).repeat(1, self.seq_len, 1)
 
-        encoded = self.pretrained(source).last_hidden_state
+        # encoded = self.pretrained(source).last_hidden_state
+        encoded = self.pretrained.encoder(source).last_hidden_state
         encoded = torch.cat((encoded, copied_morals), 2)
 
         encoded = self.linear(encoded)
@@ -56,6 +65,15 @@ class MoralTransformer(pl.LightningModule):
         
         head = self.decoder_head(decoded)
         return head
+
+    def decode_to_tokens(self, output):
+        tokens = torch.argmax(output, 2).cpu().detach().numpy()
+        results = []
+        for token_set in tokens:
+            converted = self.tokenizer.convert_ids_to_tokens(token_set)
+            results.append(converted)
+        return results
+
 
     def training_step(self, batch, batch_idx):
         return
@@ -76,10 +94,23 @@ class MoralTransformer(pl.LightningModule):
         # return optimizer
 
 if __name__ == '__main__':
-    transformer = MoralTransformer()
-    # print(transformer.pretrained)
-    source = torch.LongTensor([[0] * 512])
-    moral_target = torch.FloatTensor([[1,2,3,4,5]])
-    generated = torch.FloatTensor([[[0] * 512]* 512])
-    res = transformer.forward(source, moral_target, generated)
-    print(res.size())
+    seq_len = 128
+    batch_size = 10
+    n_intermediate = 512
+
+    transformer = MoralTransformer(seq_len=seq_len, n_intermediate=n_intermediate).cuda()
+
+    source = torch.LongTensor([list(range(seq_len))] * batch_size).cuda()
+    moral_target = torch.FloatTensor([[1,2,3,4,5]] * batch_size).cuda()
+    generated = torch.FloatTensor([[list(range(n_intermediate))] * seq_len] * batch_size).cuda()
+    outputs = transformer.forward(source, moral_target, generated)
+
+    loss = torch.sum(outputs)
+    optimizer = torch.optim.Adam(params=transformer.parameters(), lr=0.01)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # tokenized = transformer.tokenizer('trump biden electionk zx zc fzd vcx zx')['input_ids']
+    # print(tokenized)
+    # print(transformer.tokenizer.convert_ids_to_tokens(tokenized))

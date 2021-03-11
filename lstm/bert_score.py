9ab64fd5026e6e21de228d6812203d765e125988
mustafakhan14 <47877154+mustafakhan14@users.noreply.github.com>
2021-03-11 11:20:38 +0500

    take out bert score

---
+++ lstm/bert_score.py
@@ -0,0 +1,11 @@
+from datasets import load_metric
+metric = load_metric("bertscore")
+
+# @inproceedings{bert-score,
+#   title={BERTScore: Evaluating Text Generation with BERT},
+#   author={Tianyi Zhang* and Varsha Kishore* and Felix Wu* and Kilian Q. Weinberger and Yoav Artzi},
+#   booktitle={International Conference on Learning Representations},
+#   year={2020},
+#   url={https://openreview.net/forum?id=SkeHuCVFDr}
+# }
+

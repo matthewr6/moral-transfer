1bef71dca7a6d17e6df8bcd8a4b6ed3782a93811
Sasankh Munukutla <25861686+SasankhM@users.noreply.github.com>
2021-03-13 12:24:09 -0800

    hope

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def train(exp_name, gpus):
                     # callbacks=[checkpoint_callback],
                     )
                         
-    lr_finder = trainer.lr_find(model)
+    lr_finder = trainer.tuner.lr_find(model, train_loader)
     print(lr_finder.results)
     fig = lr_finder.plot(suggest=True)
     fig.show()

82b5a8d53dac73186e8282624b968ab17599271e
mustafakhan14 <47877154+mustafakhan14@users.noreply.github.com>
2021-03-14 09:13:45 +0500

    Update classifier_baseline.py

--- models/classifier_baseline.py
+++ models/classifier_baseline.py
@@ -73,7 +73,7 @@ class MoralClassifier(torch.nn.Module):
         self.embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
         self.lstm = nn.LSTM(embedding_dim, hidden_size, batch_first=True)
         self.dropout = nn.Dropout(0.2)
-        self.linear = nn.Linear(hidden_size, 11)
+        self.linear = nn.Linear(hidden_size, 10)
         self.linear1 = nn.Linear(hidden_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
 
